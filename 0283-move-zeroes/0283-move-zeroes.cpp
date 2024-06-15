ea2bc6145bf85390b5067b51c700daf1c24c5bac
Kaushik Verma <111528187+kaushik-2318@users.noreply.github.com>
2024-06-15 14:23:30 +0530

    Time: 14 ms (72.56%), Space: 21.5 MB (73.74%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+    int i = 0;
+    int j = 0;
+    while (i < nums.size() && j<nums.size())
+    {
+        if (nums[i] == 0 && nums[j] == 0)
+        {
+            j++;
+        }
+
+        else if (nums[i] == 0 && nums[j] != 0)
+        {
+            swap(nums[i], nums[j]);
+            i++;
+            j++;
+        }
+
+        else if (nums[i] != 0 && nums[j] != 0)
+        {
+            j++;
+            i++;
+        }
+        else if (nums[i] != 0 && nums[j] == 0)
+        {
+            j++;
+            i++;
+        }
+    }      
+    }
+};
