811611a9d8a22bc276f466dc20fd038a196b4d15
Kaushik Verma <111528187+kaushik-2318@users.noreply.github.com>
2024-06-25 00:44:42 +0530

    Time: 17 ms (69.71%), Space: 12.4 MB (48.44%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    string removeDuplicates(string str) {        
-      int i = 0;
-      int j = 1;
-      while (i < str.length()) {
-        if (str[i] == str[j]) {
-          str.erase(i, j-i+1);
-          i = 0;
-          j = 1;
-        } else {
-          i++;
-          j++;
-        }
-      }
-      return str;
+    string removeDuplicates(string s) {        
+      // int i = 0;
+      // int j = 1;
+      // while (i < str.length()) {
+      //   if (str[i] == str[j]) {
+      //     str.erase(i, j-i+1);
+      //     i = 0;
+      //     j = 1;
+      //   } else {
+      //     i++;
+      //     j++;
+      //   }
+      // }
+      // return str;
+          string ans = "";
+  for (int i = 0; i < s.size(); i++) {
+    if (ans.empty())
+      ans.push_back(s[i]);
+    else {
+      if (ans.back() == s[i]) {
+        ans.pop_back();
+      } else
+        ans.push_back(s[i]);
+    }
+  }
+  return ans;
     }
 };
