d659587502880cb776690abfea399ad20f5daee3
Kaushik Verma <111528187+kaushik-2318@users.noreply.github.com>
2024-08-21 19:46:25 +0530

    Time: 15 ms (15.93%), Space: 7.4 MB (23.96%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0)
+    int solve(int n){
+        if(n == 0){
             return 0;
-        if(n==1)
+        }
+        if(n==1){
             return 1;
-        return fib(n-1) + fib(n-2);  
+        }
+        return solve(n-1)+solve(n-2);
+    }
+    int fib(int n) {
+        return solve(n);
     }
 };
