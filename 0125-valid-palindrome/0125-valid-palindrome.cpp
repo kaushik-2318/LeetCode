189504cd1295b63fbcd888165fc08b0ed6002618
Kaushik Verma <111528187+kaushik-2318@users.noreply.github.com>
2024-08-21 17:36:20 +0530

    Time: 6 ms (44.92%), Space: 9.8 MB (9.40%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    char isLowerCase(char a){
-        if (a >= 65 && a <= 90) {
-            a = a + 32;
-            return a;
-        }
-        return a;
+    bool solve(string str) {
+  int s = 0;
+  int e = str.size() - 1;
+
+  while (s <= e) {
+    if (str[s] != str[e]) {
+      return false;
+    } else {
+      s++;
+      e--;
     }
-    
-    bool palindrome(string str){
-        int s = 0;
-        int e = str.size()-1;
-        
-        while(s<=e){
-            if(str[s] != str[e]){
-                return false;
-            }
-            else{
-                s++;
-                e--;
-            }
-        }
-        return true;
-    }
-    
+  }
+  return true;
+}
     bool isPalindrome(string s) {
-        for(int i = 0; i<s.length();i++){
-            s[i] = isLowerCase(s[i]);
-        }
-        string temp = "";
-        
-        for(int i = 0; i<s.length();i++){
-            if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='B' || s[i]>='0' && s[i]<='9'){
-                temp.push_back(s[i]);
-            }
-        }
-        return palindrome(temp);        
+  for (auto &x : s) {
+    x = tolower(x);
+  }
+  string str;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'B' ||
+        s[i] >= '0' && s[i] <= '9') {
+      str.push_back(s[i]);
+    }
+  }
+  return solve(str);
     }
 };
