09113959e977a627958ff83d08cab5c7067d1b3e
Kaushik Verma <111528187+kaushik-2318@users.noreply.github.com>
2025-01-18 01:00:51 +0530

    Time: 4 ms (47.66%), Space: 263.9 MB (48.13%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-void print(vector<int> arr){
-    for(int i = 0; i<arr.size();i++)
-    cout<<arr[i]<<" ";
-}
     bool doesValidArrayExist(vector<int>& derived) {
-        vector<int> ans0;
-        ans0.push_back(0);
 
-        vector<int> ans1;
-        ans1.push_back(1);
+        //Approch 1
+            // vector<int> ans0;
+            // ans0.push_back(0);
 
-        int len = derived.size();
+            // vector<int> ans1;
+            // ans1.push_back(1);
 
-        for(int i = 0; i < len-1; i++){
-            ans0.push_back(derived[i]^ans0[i]);
-            ans1.push_back(derived[i]^ans1[i]);
+            // int len = derived.size();
+
+            // for(int i = 0; i < len-1; i++){
+            //     ans0.push_back(derived[i]^ans0[i]);
+            //     ans1.push_back(derived[i]^ans1[i]);
+            // }
+
+            // if((ans0[0]^ans0[len-1] == derived[len-1]) || (ans1[0]^ans1[len-1] == derived[len-1]))
+            //     return true;
+            
+            // return false;
+
+        //Approch 2
+
+        int x = 0;
+        for(auto a: derived){
+            x = x ^ a;
         }
 
-        // print(ans0);
-        // cout<<endl;
-        // print(ans1);
-        if((ans0[0]^ans0[len-1] == derived[len-1]) || (ans1[0]^ans1[len-1] == derived[len-1]))
-            return true;
-        
-        return false;
+        return x == 0;
     }
 };
