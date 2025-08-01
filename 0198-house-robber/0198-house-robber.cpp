c042a27edcce17e191f1dd72045cc196ae62fa5f
Kaushik Verma <111528187+kaushik-2318@users.noreply.github.com>
2025-08-02 00:27:53 +0530

    Time: 0 ms (100%), Space: 10.2 MB (73.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,20 +4,20 @@ public:
 
     int helper_dp(vector<int> &v, int idx)
     {
-        if (idx == v.size() - 1)
+        if (idx == 0)
             return v[idx];
 
-        if (idx == v.size() - 2)
-            return max(v[idx], v[idx + 1]);
+        if (idx <  0)
+            return 0;
 
         if (dp[idx] != -1)
             return dp[idx];
 
-        return dp[idx] = max(v[idx] + helper_dp(v, idx + 2), 0 + helper_dp(v, idx + 1));
+        return dp[idx] = max(v[idx] + helper_dp(v, idx - 2), 0 + helper_dp(v, idx - 1));
     }
-        int rob(vector<int>& v) {
+    int rob(vector<int>& v) {
         dp.clear();
         dp.resize(105, -1);
-        return helper_dp(v, 0);
+        return helper_dp(v, v.size()-1);
     }
 };
