165f8775489235a8af4de6f7872ac71c1f48af29
Kaushik Verma <111528187+kaushik-2318@users.noreply.github.com>
2025-08-02 02:35:22 +0530

    Time: 545 ms (33.73%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,1 @@
+SELECT t1.project_id, ROUND(SUM(t2.experience_years)/COUNT(t1.project_id),2) as average_years  FROM Project as t1 LEFT JOIN Employee as t2 ON t1.employee_id = t2.employee_id GROUP BY t1.project_id order by t1.project_id asc;
