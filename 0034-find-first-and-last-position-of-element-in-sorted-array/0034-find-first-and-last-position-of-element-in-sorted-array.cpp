498f34b7b917c56309476804e2a6a5336a4b9065
Kaushik Verma <111528187+kaushik-2318@users.noreply.github.com>
2025-08-07 17:39:07 +0530

    Time: 0 ms (100%), Space: 18.1 MB (17.25%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
-    int floor(vector<int>& arr, int target){
+    int first(vector<int>& arr, int t){
+        int n = arr.size();
         int l = 0;
-        int h = arr.size()-1;
+        int r = n-1;
         int ans = -1;
-        while(l<=h){
-            int mid = (l + h)/2;
-            if(arr[mid] == target){
+        while(l <= r){
+            int mid = (l+r)/2;
+            if(arr[mid] == t){
                 ans = mid;
-                h = mid-1;
-            }
-            else if(arr[mid] < target){
-                l = mid +1;
-            }
-            else{
-                h = mid-1;
+                r = mid - 1;
+            }else if(arr[mid] < t){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
             }
         }
         return ans;
     }
-
-    int ceil(vector<int>& arr, int target){
-    int l = 0;
-    int h = arr.size()-1;
-    int ans = -1;
-        while(l<=h){
-            int mid = (l + h)/2;
-            if(arr[mid] == target){
+    int last(vector<int> arr, int t){
+        int n = arr.size();
+        int l = 0;
+        int r = n-1;
+        int ans = -1;
+        while(l <= r){
+            int mid = (l+r)/2;
+            if(arr[mid] == t){
                 ans = mid;
-                l = mid+1;
+                l = mid + 1;
             }
-            else if(arr[mid] < target){
-                l = mid +1;
-            }
-            else{
-                h = mid-1;
+            else if(arr[mid] > t){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
             }
         }
         return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size() == 0) return {-1,-1};
-        int l = floor(nums, target);
-        int h = ceil(nums, target);
-        return {l,h};
-
+        int a = first(nums, target);
+        int b = last(nums, target);
+        return {a,b};
     }
 };
